33bb7accd988c8b4289fa6e9c8a6b6faba37fbd2
Igor A. Vilhalba <igor.vilhalba@g.globo>
2026-07-19 16:54:24 -0300

    Add Missing license file

---
+++ lib/src/androidTest/java/io/igrvlhlb/lib/ExampleInstrumentedTest.kt
@@ -0,0 +1,31 @@
+package io.igrvlhlb.lib
+/*
+ * This file is part of the Codec(i) project
+ *
+ * Copyright (C) 2025 Igor A. Vilhalba
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("io.igrvlhlb.lib.test", appContext.packageName)
+    }
+}
---
+++ lib/src/test/java/io/igrvlhlb/lib/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package io.igrvlhlb.lib
+/*
+ * This file is part of the Codec(i) project
+ *
+ * Copyright (C) 2025 Igor A. Vilhalba
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
